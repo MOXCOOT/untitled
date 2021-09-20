101f00b0381b399a0aa98475e3bba4dd0991c3a4
MoxCoot <moxcoot@gmail.com>
2021-09-20 17:34:34 +0800

    Changes

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -3,5 +3,6 @@ public class HelloWord {
         System.out.println("Hello World");
         System.out.println("A New Version");
         System.out.println("Yes");
+        System.out.println("KKK");
     }
 }
