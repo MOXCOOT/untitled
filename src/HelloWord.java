f88e6abed6c9f67bc1b354aa00bebdda5ba26045
MoxCoot <moxcoot@gmail.com>
2021-09-23 14:56:01 +0800

    Changes

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,5 +1,6 @@
 public class HelloWord {
     public static void main(String[] args) {
+        String s;
         System.out.println("Hello World");
         System.out.println("A New Version");
         System.out.println("Yes");
